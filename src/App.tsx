13e7a12b5addff18d8b81056c5481b9113be2ae1
Kai Yamada <gpioblink@aol.com>
2020-02-20 13:55:51 +0900

    finish making screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import InboxScreen from './components/IndoxScreen';
+
 function App() {
   return (
     <div className="App">
+      <InboxScreen />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/IndoxScreen.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { PureInboxScreen } from "./IndoxScreen";
+import { Provider } from "react-redux";
+import {defaultTasksData} from "./TaskList.stories";
+
+export default {
+    component: PureInboxScreen,
+    title: 'InboxScreen',
+    decorators: [story => <Provider store={store}>{story()}</Provider>],
+};
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+      return {
+          tasks: defaultTasksData,
+      };
+  },
+    subscribe: () => 0,
+    dispatch: action('dispatch'),
+};
+
+export const Default = () => <PureInboxScreen />;
+export const Error = () => <PureInboxScreen error="Something" />;
