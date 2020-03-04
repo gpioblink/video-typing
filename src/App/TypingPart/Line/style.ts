6ac498356c48cab7d00f4c3a4ff5015b725eb36d
Kai Yamada <gpioblink@aol.com>
2020-03-04 23:04:24 +0900

    remove pixel definition

--- src/App/TypingPart/Line/style.ts
+++ src/App/TypingPart/Line/style.ts
@@ -2,10 +2,10 @@ import styled from '@emotion/styled'
 
 export const Style = styled.div`
 background-color: #F0F0F0;
-height: 100px;
+height: 13vw;
 display: grid;
-grid-template-columns: repeat(50, 20px);
+grid-template-columns: repeat(50, 2.5vw);
 grid-template-rows: [text-start] 40% [line-start] 10% [tag-start] auto;
 font-family: monospace;
-font-size: 36px;
+font-size: 5vw;
 `;
