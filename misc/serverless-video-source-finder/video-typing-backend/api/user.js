3c4348b87edf6ab450c80f5de727029719c0c838
Kai Yamada <gpioblink@aol.com>
2020-03-08 00:44:19 +0900

    add service controller

--- misc/serverless-video-source-finder/video-typing-backend/api/user.js
+++ misc/serverless-video-source-finder/video-typing-backend/api/user.js
@@ -7,6 +7,84 @@ AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+const lambda = new AWS.Lambda({
+  region: process.env.LAMBDA_REGION
+});
+
+module.exports.getVideoService = async (event, context) => {
+  const cache = await invokeCacheVideo(event.body);
+  console.log("cache_res", cache);
+
+  if(cache['Payload']) {
+    const payload = JSON.parse(cache['Payload']);
+    if(payload.body) {
+      const body = JSON.parse(payload.body);
+      console.log("body_cache_res", body);
+      if(!body['error'] && body['source']) {
+        return {
+          statusCode: 200,
+          headers: {'Access-Control-Allow-Origin': '*'},
+          body: JSON.stringify({'res': JSON.parse(body['source']), 'type': 'cache'})
+        };
+      }
+    }
+  }
+
+  const real = await invokeRealVideo(event.body); 
+  console.log("real_res_obj", real);
+
+  if(real['Payload']) {
+    const payload = JSON.parse(real['Payload']);
+    if(payload.body) {
+      const body = JSON.parse(payload.body);
+      if(body['res']) {
+        await saveCacheVideo(JSON.stringify({'url': JSON.parse(event.body).url, 'source': JSON.stringify(body.res)}));
+        return {
+          statusCode: 200,
+          headers: {'Access-Control-Allow-Origin': '*'},
+          body: JSON.stringify({'res': body['res'], 'type': 'real'})
+        }
+      };
+    }
+  }
+
+  return {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({'error': `no available video source was found`, 'type': 'error'})
+  };
+};
+
+const invokeCacheVideo = body => {
+  const params = {
+    FunctionName: process.env.CACHE_VIDEO_SOURCE_LAMBDA_NAME,
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({'body': body})
+  };
+  console.log("cachevideo", params);
+  return lambda.invoke(params).promise();
+};
+
+const invokeRealVideo = body => {
+  const params = {
+    FunctionName: process.env.REAL_VIDEO_SOURCE_LAMBDA_NAME,
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({'body': body})
+  };
+  console.log("invokevideo", params);
+  return lambda.invoke(params).promise();
+};
+
+const saveCacheVideo = body => {
+  const params = {
+    FunctionName: process.env.SAVE_VIDEO_SOURCE_LAMBDA_NAME,
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({'body': body})
+  };
+  console.log("savevideo", params);
+  return lambda.invoke(params).promise();
+};
+
 module.exports.saveVideoCache = (event, context, callback) => {
   const requestBody = JSON.parse(event.body);
   const siteUrl = requestBody.url;
@@ -90,12 +168,27 @@ module.exports.getVideoCache = (event, context, callback) => {
 
   dynamoDb.get(params).promise()
    .then(result => {
+     const body = result.Item;
+     const currentTime = new Date().getTime()
+     // 該当なしや、前回取得時から3時間以上経過しているものは返さない
+     if(!body['updatedAt'] || currentTime - parseInt(body['updatedAt']) > 10800000) {
+      const response = {
+        statusCode: 200,
+        headers: {
+         'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({'error': 'not found or expired'}),
+      };
+      callback(null,response);
+      return;
+     }
+
      const response = {
        statusCode: 200,
        headers: {
         'Access-Control-Allow-Origin': '*',
        },
-       body: JSON.stringify(result.Item),
+       body: JSON.stringify(body),
      };
      callback(null, response);
    })
