5fbd69c0e48d188d19cd64b3ac66de2e61380b69
Kai Yamada <gpioblink@aol.com>
2020-03-07 16:33:04 +0900

    add saving video sources

--- misc/serverless-video-source-finder/video-typing-backend/api/user.js
+++ misc/serverless-video-source-finder/video-typing-backend/api/user.js
@@ -1,6 +1,42 @@
 'use strict';
 
-module.exports.getvideo = async event => {
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.getvideo = (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+  const siteUrl = requestBody.url;
+  const sourceJson = requestBody.source;
+
+  if(typeof siteUrl !== 'string' || typeof sourceJson !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t submit video source because of validation errors.'));
+    return;
+  }
+
+  submitVideoSourceP(videoSourceInfo(siteUrl,sourceJson))
+   .then(res => {
+     callback(null, {
+       statusCode: 200,
+       body: JSON.stringify({
+         message: `Sucessfully submitted video source with siteUrl ${siteUrl}`
+       })
+     });
+   })
+   .catch(err => {
+     console.log(err);
+     callback(null, {
+       statusCode: 500,
+       body: JSON.stringify({
+         message: `Unable to submit video source with siteUrl ${siteUrl}`
+       })
+     });
+   })
+
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -12,7 +48,25 @@ module.exports.getvideo = async event => {
       2
     ),
   };
-
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+const submitVideoSourceP = videoSource => {
+  console.log('Submitting video source');
+  const videoSourceInfo = {
+    TableName: process.env.VIDEO_TYPING_TABLE,
+    Item: videoSource,
+  };
+  return dynamoDb.put(videoSourceInfo).promise()
+   .then(res => videoSource);
+};
+
+const videoSourceInfo = (siteUrl, sourceJson) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: siteUrl,
+    source: sourceJson,
+    updatedAt: timestamp,
+  };
 };
